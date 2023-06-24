5f80c9f777f76336af4b3a93110572a6ab947be1
Nishit Singh Gaur <nishit.g@brilworks.com>
2023-06-24 11:52:47 +0530

    Auth folder with Login/Singup Component with routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import './Styles/style.scss';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
