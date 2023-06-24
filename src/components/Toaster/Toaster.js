fd97695387887c530c26311ae6d12d241dd666b8
Parth savaliya <parth.s@brilworks.com>
2023-06-24 19:45:44 +0530

    updaeted data

--- src/components/Toaster/Toaster.js
+++ src/components/Toaster/Toaster.js
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Toast, ToastContainer } from "react-bootstrap";
 
 const Toaster = ({ data }) => {
-  const { message, type } = data;
-  
+  const { message, type, heading } = data;
+  const [show, setShowToast] = useState(false);
+
+  useEffect(() => {
+    setShowToast(!show);
+  }, [data]);
 
   return (
     <ToastContainer position="bottom-end" className="me-2 mb-2">
-      <Toast show={!!data} bg={type.toLowerCase()}>
+      <Toast show={show} bg={type} onClose={() => setShowToast(false)}>
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-          <strong className="me-auto">{type}</strong>
+          <strong className="me-auto">{heading}</strong>
         </Toast.Header>
         <Toast.Body>{message}</Toast.Body>
       </Toast>
