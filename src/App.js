7cf732e439df5cc5228d25b26cdf973ac3665ab7
Nishit Singh Gaur <nishit.g@brilworks.com>
2023-06-24 15:41:51 +0530

    home component v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Styles/style.scss';
-import Login from './components/Auth/Login';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <>
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
